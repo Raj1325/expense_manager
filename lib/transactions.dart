c9b18b5198f2a6cf5f6d56ca8d24f95d9b34f747
Raj Dighe <rppdighe4892@gmail.com>
2021-01-04 20:02:09 +1100

    BASIC SETUP

---
+++ lib/transactions.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import "package:flutter/foundation.dart";
+
+class Transaction {
+  int id;
+  String title;
+  double amount;
+  DateTime time;
+
+  Transaction(
+      {@required this.id,
+      @required this.amount,
+      @required this.time,
+      @required this.title});
+}
